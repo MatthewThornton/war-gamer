9d38fe1bbea8321e4bad014c3ba5b0bbdb6c85b7
Matthew Thornton <matthewthornton.usa@gmail.com>
2022-09-09 11:53:39 -0700

    Update FB-Auth from Amplify"

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,6 @@ export type AmplifyDependentResourcesAttributes = {
         "wargamer": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
